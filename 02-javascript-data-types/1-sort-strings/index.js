1df809d7d4a385984ce5bf2eb77a8e8bc2ebc113
ruslan <ruslangr@yahoo.com>
2020-09-01 14:34:07 +0300

    add Pick()

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,14 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let sort_direction = (param == 'asc') ? 1 : -1;
+  let new_arr = arr.slice();
+  return new_arr.sort((a, b) => {
+    if (a.toUpperCase().localeCompare(b.toUpperCase()) == 0) {
+      return -a.localeCompare(b) * sort_direction;
+    } else {
+      return a.localeCompare(b) * sort_direction;
+    }
+  });
 }
+
