2df992c70a3c0cffbbd5019e879fd99546f6c22c
ruslan <ruslangr@yahoo.com>
2020-09-02 20:46:47 +0300

    add trimSymbols()

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,11 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let uniques = new Set(string.split(''));
+  for(let char of uniques){
+    let re_str = `${char}{${size},}`;
+    let re = new RegExp(re_str, "gi");
+    string = string.replace(re, char.repeat(size));
+  }
+  return string;
 }
