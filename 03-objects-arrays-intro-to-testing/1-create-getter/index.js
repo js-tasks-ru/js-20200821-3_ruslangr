9812510c3a31a64dd6d84dc944d82835f9291dc9
ruslan <ruslangr@yahoo.com>
2020-09-02 18:27:30 +0300

    add createGetter()

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  let props = path.split('.');
+  return  (obj) => {
+    let val = obj[props.shift()];
+    if(val === undefined) return undefined;
+    for(let prop of props){
+      val = val[prop];
+    }
+    return val;
+  }
 }
